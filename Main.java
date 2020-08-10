1ade9c336be6fc9b6e7d3c645f6f0e120bbb3e90
WuAlicja <69319294+WuAlicja@users.noreply.github.com>
2020-08-10 21:59:24 +0200

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         List<Powerlifter> powerLifters=new ArrayList<>();
+
         Powerlifter player1=new RawPowerLifter("Piotr","Domanski",
                 10,10,10);
         powerLifters.add(player1);
